3cfea7ed9c7c8a62a782324ac9bc3862ff07ce0c
Juan-Little <juanbenitodevalle@gmail.com>
2023-01-16 14:25:54 +0100

    principiode bucle con Andres

---
+++ bucle.js
@@ -0,0 +1,15 @@
+let caja1 = document.getElementById("valor1");
+let valor1 = caja1.value;
+let caja2 = document.getElementById("valor2");
+let valor2 = caja1.value;
+
+ function bucle(inicio,tope) {
+
+    for (let i = inicio; i <= tope; i++) {
+      console.log(i);
+    }
+//     return numberSelected;
+  }
+
+bucle(valor1,valor2);
+
